ce3c3c704b4f979cc92e0aa20b853780cfce401c
Harley Rayner <harleymteden@gmail.com>
2018-02-12 17:43:48 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 class Cat
-  def initialize
-    @name = name
-    @fur_color = fur_color
-  end
+  # def initialize
+  #   @name = name
+  #   @fur_color = fur_color
+  # end
   attr_accessor :name
   attr_accessor :fur_color
   def meow
